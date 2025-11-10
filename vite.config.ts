c907cd9069e056589a3f2e2a23ce2f9f98e84c85
forrigo <fabricio@orrigo.com.br>
2025-11-10 10:20:46 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    // Pass environment variables to the client-side code
-    'process.env.API_KEY': JSON.stringify(process.env.VITE_GEMINI_API_KEY)
-  },
   server: {
     port: 5173
   }
