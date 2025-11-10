6133a3abf24e41d487500d8237b8003b77b3c77b
forrigo <fabricio@orrigo.com.br>
2025-11-09 23:58:26 -0300

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,24 @@
 /// <reference types="vite/client" />
+
+declare namespace google {
+    namespace accounts {
+        namespace oauth2 {
+            function initTokenClient(config: TokenClientConfig): TokenClient;
+            function revoke(token: string, callback: () => void): void;
+
+            interface TokenClient {
+                requestAccessToken: () => void;
+            }
+
+            interface TokenClientConfig {
+                client_id: string;
+                scope: string;
+                callback: (tokenResponse: TokenResponse) => void;
+            }
+
+            interface TokenResponse {
+                access_token: string;
+            }
+        }
+    }
+}
