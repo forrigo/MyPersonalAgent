683dce3abc1b3a45771dfa0ea5aa01a562bf47a0
forrigo <fabricio@orrigo.com.br>
2025-11-10 00:14:27 -0300

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -20,13 +20,13 @@ export interface AgendaItem {
     id: string;
     title: string;
     time?: string;
-    type: ItemType.AGENDA;
+    type: 'AGENDA';
 }
 
 export interface TodoItem {
     id: string;
     title: string;
-    type: ItemType.TODO;
+    type: 'TODO';
 }
 
 export interface GoogleUser {
